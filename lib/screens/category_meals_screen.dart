62aab1e13c34ec10b8fcccd7960395894209af64
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-09 22:27:15 +0200

    9. Start Building CategoryMealsScreen Design

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('The Recipes')),
+      body: const Center(
+        child: Text('The Recipes For The Category'),
+      ),
+    );
+  }
+}
