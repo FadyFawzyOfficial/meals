edf1a0f251554973345ac38d86f34a50919bad57
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-09 20:53:09 +0200

    3. Start Building CategoriesScreen & GridView for Categories

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+      children: const [],
+    );
+  }
+}
