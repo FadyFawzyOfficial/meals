802904aef2fd6eaa4b0b6d360231d590deb7f799
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-09 22:31:30 +0200

    1. Navigate to a New Screen (How to?)

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -9,23 +10,31 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(16)),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.7),
-            category.color,
-          ],
-          begin: AlignmentDirectional.topStart,
-          end: AlignmentDirectional.bottomEnd,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: AlignmentDirectional.topStart,
+            end: AlignmentDirectional.bottomEnd,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleMedium,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
+
+  void selectCategory(BuildContext context) => Navigator.push(
+      context, MaterialPageRoute(builder: (_) => const CategoryMealsScreen()));
 }
