5c41ecce888c53c0379d61a8a38ef9d7904f49b7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-11 12:17:58 +0200

    3. Use BottomNavigationBar to Navigate (Add a Bottom TabBar)

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,36 +3,52 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorites_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _screens = [
+    {
+      'screen': const CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'screen': const FavoritesScreen(),
+      'title': 'Your Favorites',
+      // 'actions': [IconButton(onPressed: () {}, icon: const Icon(Icons.delete))]
+    },
+  ];
+
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('DeliMeals'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Category',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(title: Text(_screens[_currentIndex]['title'] as String)),
+      body: _screens[_currentIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
           ),
-        ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
       ),
     );
   }
+
+  void _onTap(int index) => setState(() => _currentIndex = index);
 }
